17a747cb7af73db7b45e660eb46e0e98418d4c93
brysommer <brysommer1@gmail.com>
2026-03-13 16:05:31 +0200

    wrong import address

--- src/router/bot-transaction.ts
+++ src/router/bot-transaction.ts
@@ -1,6 +1,6 @@
 import e from 'express';
 import { createTransaction, findTransaction } from '../controllers/bot-transactions';
-import getUserTransactions from 'controllers/user-tarnsactions';
+import getUserTransactions from '../controllers/user-tarnsactions';
 
 export default (router: e.Router) => {
     router.post('/transactions/create', createTransaction);
