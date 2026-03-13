f0fe8fbdeab5160cc4ccbc76a09df2a62e8f2b5e
brysommer <brysommer1@gmail.com>
2026-03-13 16:39:52 +0200

    remove timezone from crone processes

--- src/index.ts
+++ src/index.ts
@@ -22,44 +22,20 @@ server.listen(8080, () => {
     console.log(`Server running on http://localhost:8080/`);
 });
 
-nodeCron.schedule(
-    '*/10 * * * *',
-    () => {
-        import10minTransactions().catch(console.error);
-    },
-    {
-        timezone: 'Europe/Kiev', // щоб опівночі за київським часом
-    },
-);
+nodeCron.schedule('*/10 * * * *', () => {
+    import10minTransactions().catch(console.error);
+});
 
-nodeCron.schedule(
-    '0 0 * * *',
-    () => {
-        daySummoryLog().catch(console.error);
-    },
-    {
-        timezone: 'Europe/Kiev', // щоб опівночі за київським часом
-    },
-);
+nodeCron.schedule('0 0 * * *', () => {
+    daySummoryLog().catch(console.error);
+});
 
-nodeCron.schedule(
-    '0 0 * * 0',
-    () => {
-        // Щонеділі о 00:00
-        botWeeklyUsersStatistic();
-    },
-    {
-        timezone: 'Europe/Kiev',
-    },
-);
+nodeCron.schedule('0 0 * * 0', () => {
+    // Щонеділі о 00:00
+    botWeeklyUsersStatistic();
+});
 
-nodeCron.schedule(
-    '0 0 1 * *',
-    () => {
-        // 1-го числа кожного місяця о 00:00
-        botMonthlyUsersStatistic();
-    },
-    {
-        timezone: 'Europe/Kiev',
-    },
-);
+nodeCron.schedule('0 0 1 * *', () => {
+    // 1-го числа кожного місяця о 00:00
+    botMonthlyUsersStatistic();
+});
